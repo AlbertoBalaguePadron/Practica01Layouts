03e4073aed9a105a452aaf08b3d95980562343a8
Alberto <alberto01bp@gmail.com>
2022-10-05 22:24:16 +0100

    Feature: Agrego el Qr direccionando a mi página personal de git

--- components/body.js
+++ components/body.js
@@ -1,19 +1,25 @@
+import SvgQRCode from "react-native-qrcode-svg";
 import { Text, View, StyleSheet } from "react-native";
 
 const body = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.texto}>
-        Mi nombre es Alberto Balagué Padrón, actualmente soy un alumno de los Salesianos
-        La Cuesta. Me gusta saber de todo soy muy curioso aparte tambien me gusta jugar.
+        Mi nombre es Alberto Balagué Padrón, actualmente soy un alumno de los
+        Salesianos La Cuesta. Me gusta saber de todo soy muy curioso aparte
+        tambien me gusta jugar.
       </Text>
-      
+      <SvgQRCode
+        value="https://github.com/AlbertoBalaguePadron/Practica01Layouts/tree/main"
+        style={styles.qr}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
     width: "76%",
     flexDirection: "column",
     justifyContent: "space-between",
